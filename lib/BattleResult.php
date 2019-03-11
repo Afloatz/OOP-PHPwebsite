cffcbcb5d12df58adb655567120283d1a3061176
Afloatz <florentlaot.fr@gmail.com>
2019-03-11 16:33:42 +0100

    Optional type-hinting

--- lib/BattleResult.php
+++ lib/BattleResult.php
@@ -14,7 +14,7 @@ class BattleResult
 
     private $losingShip;
 
-    public function __construct($usedJediPowers, Ship $winningShip, Ship $losingShip)
+    public function __construct($usedJediPowers, Ship $winningShip = null, Ship $losingShip = null)
     {
         $this->usedJediPowers = $usedJediPowers;
         $this->winningShip = $winningShip;
@@ -30,7 +30,7 @@ class BattleResult
     }
 
     /**
-     * @return Ship
+     * @return Ship|null
      */
     public function getWinningShip()
     {
@@ -38,11 +38,17 @@ class BattleResult
     }
 
     /**
-     * @return Ship
+     * @return Ship|null
      */
     public function getLosingShip()
     {
         return $this->losingShip;
+    }
+
+    public function isThereAWinner()
+    {
+        return $this->getWinningShip() !== null;
+
     }
 
 }
